a792383bd561be928147f5e514e21763d14681d2
bastien_luddecke <bastien_luddecke@calypso.com>
2016-03-24 14:45:21 +0100

    new job for release and production deployment

--- infra/ansible/roles/jenkins/templates/release-pipeline.groovy
+++ infra/ansible/roles/jenkins/templates/release-pipeline.groovy
@@ -1,20 +1,27 @@
 #!groovy
 import groovy.json.JsonSlurper
 
-def uatMaster = "10.110.192.200"
-def uatProxy = "10.110.192.200"
+def jenkins = "10.100.192.200"
+def prodMaster = "10.120.192.200"
+def prodProxy = "10.120.192.200"
 
 def repo = "bludek/click-paradise"
 def service = "clickcount"
 def servers
 
 node {
-    // Mark the code checkout 'stage'....
+    stage 'Check'
+    def result = getJsonService("http://${jenkins}:8080/job/click-count/api/json")
+    def lastSuccess = result.lastSuccessfulBuild.number
+    println "Last successful build is : ${lastSuccess}"
+    result = getJsonService("http://${jenkins}:8080/job/click-count/${lastSuccess}/api/json")
+    def revision = result.actions.lastBuiltRevision.SHA1[0]
+    println "Last successful revision is : ${revision}"
+
     stage 'Checkout'
-    // servers = load 'servers.groovy'
     checkout(
         [$class: 'GitSCM',
-        branches: [[name: '*/master']],
+        branches: [[name: "${revision}"]],
         doGenerateSubmoduleConfigurations: false,
         extensions: [[
             $class: 'CloneOption',
@@ -25,48 +32,30 @@ node {
         submoduleCfg: [],
         userRemoteConfigs: [[url: 'https://github.com/bludek/click-paradise.git']]  ]
     )
-
-    // Mark the code build 'stage'....
-    stage 'Build'
-    // Run the maven build
-    mvn 'clean package'
     dir('.') {stash name: 'compose', includes: '*.yml'}
-    step([$class: 'Fingerprinter', targets: 'target/*.war'])
 
-    stage 'QA'
-    parallel(longerTests: {
-        runTests(servers, 30)
-    }, quickerTests: {
-        runTests(servers, 20)
-    })
+    stage 'Maven Release'
+    //extract the current version
+    def pom = readFile 'pom.xml'
+    def currentVersion = new XmlParser().parseText(pom).version.text()
+    currentVersion = currentVersion.replaceAll('-SNAPSHOT', '')
+    // TODO: perform maven release, upgrade version
 
+    stage 'Docker release'
     docker.withRegistry('http://10.100.192.200:5000') {
-        stage 'Build Docker image'
-        def img = docker.build("clickparadise/clickcount:${env.BUILD_TAG}")
-
-        stage name: 'Promote Image', concurrency: 1
-        // All the tests passed. We can now retag and push the 'latest' image.
-        img.push('staging');
+        def img = docker.image("clickparadise/clickcount:jenkins-click-count-${lastSuccess}")
+        img.push("production")
+        img.push("${currentVersion}")
     }
-
 }
 
-node("uat") {
-    stage name: 'UAT Deployment', concurrency: 1
-    def nextColor = deploy("uat", "${uatMaster}", "${service}")
 
-    stage name: 'UAT Post-Deployment', concurrency: 1
-    postDeploy("uat", "${uatMaster}", "${uatProxy}", "${nextColor}", "${service}")
-}
+node("prod") {
+    stage name: 'Deployment', concurrency: 1
+    def nextColor = deploy("prod", "${prodMaster}", "${service}")
 
-def mvn(args) {
-    sh "${tool 'M3'}/bin/mvn ${args}"
-}
-
-def runTests(servers, duration) {
-    node {
-
-    }
+    stage name: 'Post-Deployment', concurrency: 1
+    postDeploy("prod", "${prodMaster}", "${prodProxy}", "${nextColor}", "${service}")
 }
 
 def deploy(id, master, service) {
@@ -111,7 +100,6 @@ def waitFor(address) {
         def req = "curl -X GET ${address}/clickCount/rest/healthcheck".execute().text
         isUp = req == "ok"
         if (!isUp) {
-            println req
             def proc = "sleep 3".execute()
             proc.waitFor()
         }
@@ -147,4 +135,10 @@ def getAddress(swarmMaster, service, color) {
 
 def getInstances(swarmMaster, service) {
     return 1
+}
+
+def getJsonService(url) {
+    def serviceJson = "${url}".toURL().text
+    def result = new JsonSlurper().parseText(serviceJson)
+    return result
 }
